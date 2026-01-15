dcaa3baa2d96944cc044fe9a42882f49d07ae599
inschrift.spruch.raum <inschrift.spruch.raum@outlook.com>
2026-01-16 00:44:31 +0800

    [new]nvidia-cdi-generate service

--- config/pkgs/default.nix
+++ config/pkgs/default.nix
@@ -45,4 +45,48 @@ in
     daemon.settings.features.cdi = true;
     daemon.settings.cdi-spec-dirs = ["/etc/cdi"];
   };
+
+  systemd.tmpfiles.rules = [
+    "d /etc/cdi 0755 root root -"
+  ];
+
+  systemd.services.nvidia-cdi-generate = {
+    description = "Generate NVIDIA CDI specification for container runtime";
+    
+    wantedBy = ["multi-user.target"];
+    
+    before = ["docker.service"];
+    
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      
+      Environment = "PATH=/run/current-system/sw/bin";
+      
+      ExecStart = ''
+        ${pkgs.nvidia-container-toolkit}/bin/nvidia-ctk cdi generate \
+          --output=/etc/cdi/nvidia.yaml \
+          --library-search-path=/run/opengl-driver/lib
+      '';
+      
+      User = "root";
+      Group = "root";
+      
+      Restart = "on-failure";
+      RestartSec = "5s";
+      
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+  };
+
+  services.udev.extraRules = ''
+    ACTION=="add|change", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03*", \
+      TAG+="systemd", ENV{SYSTEMD_WANTS}="nvidia-cdi-generate.service"
+    
+    ACTION=="add|change", SUBSYSTEM=="misc", KERNEL=="nvidia[0-9]*", \
+      TAG+="systemd", ENV{SYSTEMD_WANTS}="nvidia-cdi-generate.service"
+  '';
+
+  systemd.services.docker.after = ["nvidia-cdi-generate.service"];
 }
